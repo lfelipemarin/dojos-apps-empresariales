cae6b569bff02a712dc1649490698b0009201f58
Felipe Marin <lfelipe.marin@gmail.com>
2016-04-02 12:42:46 -0500

    Primer Dojo: 1. Scala, 2. Angular2.

---
+++ Dojo 2- Angular2 - Heroes/app/mock-heroes.js
@@ -0,0 +1,25 @@
+System.register([], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var Heroes;
+    return {
+        setters:[],
+        execute: function() {
+            exports_1("Heroes", Heroes = [
+                { "id": 11, "name": "Batman" },
+                { "id": 12, "name": "Superman" },
+                { "id": 13, "name": "Profesor super O" },
+                { "id": 14, "name": "Acuaman" },
+                { "id": 15, "name": "Flash" },
+                { "id": 16, "name": "Linterna Verde" },
+                { "id": 17, "name": "Deadpool" }
+            ]);
+        }
+    }
+});
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/ 
+//# sourceMappingURL=mock-heroes.js.map
---
+++ Dojo 2- Angular2 - Heroes/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import {Hero} from './hero';
+
+export var Heroes: Hero[] = [
+    { "id": 11, "name": "Batman" },
+    { "id": 12, "name": "Superman" },
+    { "id": 13, "name": "Profesor super O" },
+    { "id": 14, "name": "Acuaman" },
+    { "id": 15, "name": "Flash" },
+    { "id": 16, "name": "Linterna Verde" },
+    { "id": 17, "name": "Deadpool" }
+];
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
